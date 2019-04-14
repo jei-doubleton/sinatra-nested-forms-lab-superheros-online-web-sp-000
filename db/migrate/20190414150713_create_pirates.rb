5532ba96279b6ea12434171bdd4ce878e7816699
J. Singleton <jennifer.singleton@gmail.com>
2019-04-14 15:09:26 +0000

    Automatically backed up by Learn

--- db/migrate/20190414150713_create_pirates.rb
+++ db/migrate/20190414150713_create_pirates.rb
@@ -2,7 +2,7 @@ class CreatePirates < ActiveRecord::Migration[5.2]
   def change
     create_table :pirates do |t|
       t.string :name
-      t.string :weight
       t.string :height
+      t.string :weight
   end
 end
